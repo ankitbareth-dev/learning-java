8d682aa649b5aed5d0b727a0791b212e811399b9
ankitbareth-dev <ankitbareth.dev@gmail.com>
2025-10-17 16:26:16 +0530

    2D array

--- Chapter-1/hello.java
+++ Chapter-1/hello.java
@@ -1,5 +1,32 @@
-class hello {
-  public static void main(String a[]) {
-    System.out.print("Hello, World");
+public class hello {
+
+  static int[][] matrixAddition(int[][] a, int[][] b) {
+    int rows = a.length;
+    int cols = a[0].length;
+   
+    int[][] result = new int[rows][cols];
+
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        result[i][j] = a[i][j] + b[i][j];
+      }
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int[][] a = {{1, 2}, {3, 4}};
+    int[][] b = {{5, 6}, {7, 8}};
+
+    int[][] sum = matrixAddition(a, b);
+
+    System.out.println("Result of Matrix Addition:");
+    for (int i = 0; i < sum.length; i++) {
+      for (int j = 0; j < sum[i].length; j++) {
+        System.out.print(sum[i][j] + " ");
+      }
+      System.out.println();
+    }
   }
 }
